1ee9671f2a4a720582e9f78257ee36860e02baf8
jonathanelscpt <jonathanelscpt@gmail.com>
2019-12-29 15:58:51 +0100

    fixed python_requires

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
     keywords=[
         'VALR', 'REST', 'API', 'Bitcoin', 'Ethereum',
     ],
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*',
+    python_requires='>=3.6',
     install_requires=[
        'requests',
     ],
