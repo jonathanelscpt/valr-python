7d4afaf4c657d08e09d3ccbee237916f3e37f097
jonathanelscpt <jonathanelscpt@gmail.com>
2020-01-03 00:50:03 +0100

    unit test refactoring

---
+++ tests/test_request_signature.py
@@ -0,0 +1,26 @@
+import json
+from valr_python.base_client import _sign_request  # noqa
+
+
+def test_request_signature_basic():
+    api_secret = 'superdupersecret'
+    timestamp = 1577572690093
+    method = 'DELETE'
+    path = '/v1/orders/order'
+    data = {
+        "orderId": "UUID",
+        "pair": "BTCZAR"
+    }
+    body = json.loads(json.dumps(data))
+    signature = _sign_request(api_secret=api_secret, timestamp=timestamp, method=method, path=path, body=body)
+    assert signature == '862ab6527f1ec72bb2243e5f01ae66515d0e74ef1e36aa68c031c045df1b3b62bd43858642a8425368895354e360715add8e3aec47432ea69f60bf6cbd546ea5'  # noqa
+
+
+def test_request_signature_empty_body():
+    api_secret = 'superdupersecret'
+    timestamp = 1577572690093
+    method = 'GET'
+    path = '/v1/account/balances'
+    body = ''
+    signature = _sign_request(api_secret=api_secret, timestamp=timestamp, method=method, path=path, body=body)
+    assert signature == '647d276537b952fe37f349422a4a60a76ecc2e3fad509a523b03dccd1a940525f8ff06314ad1adc5625000223c514637cd9682ee89ffc285b7493e7c64e746aa'  # noqa
--- tests/test_valr_client.py
+++ tests/test_valr_client.py
@@ -2,7 +2,6 @@ import pytest
 import requests_mock
 
 from valr_python import Client
-from valr_python.base_client import _sign_request  # noqa
 from valr_python.error import APIError
 from valr_python.error import RequiresAuthentication
 
@@ -14,7 +13,7 @@ def mock_client():
     return c
 
 
-def test_client(mock_client):
+def test_client_attrs(mock_client):
     mock_client.api_secret = 'api_secret'
     mock_client.api_key = 'api_key'
     mock_client.base_url = 'base_url'
@@ -55,29 +54,19 @@ def test_client_do_basic(mock_client):
 
     adapter.register_uri('GET', 'mock://test/', text='ok')
     with pytest.raises(Exception):
-        res = mock_client._do('GET', '/')
+        mock_client._do('GET', '/')
 
     adapter.register_uri('GET', 'mock://test/', text='{"key":"value"}')
     res = mock_client._do('GET', '/')
     assert res['key'] == 'value'
 
     adapter.register_uri('GET', 'mock://test/', text='{}', status_code=400)
-    res = mock_client._do('GET', '/')  # no exception, because no error present
+    mock_client._do('GET', '/')  # no exception, because no error present
 
     adapter.register_uri('GET', 'mock://test/',
                          text='{"error_code":"code","error":"message"}',
                          status_code=400)
     with pytest.raises(APIError) as e:
-        res = mock_client._do('GET', '/')
+        mock_client._do('GET', '/')
     assert e.value.code == 'code'
     assert e.value.message == 'message'
-
-
-def test_request_signature_basic():
-    api_secret = 'superdupersecret'
-    timestamp = 1577572690093
-    method = 'GET'
-    path = '/v1/account/balances'
-    body = ''
-    signature = _sign_request(api_secret=api_secret, timestamp=timestamp, method=method, path=path, body=body)
-    assert signature == '647d276537b952fe37f349422a4a60a76ecc2e3fad509a523b03dccd1a940525f8ff06314ad1adc5625000223c514637cd9682ee89ffc285b7493e7c64e746aa'  # noqa
