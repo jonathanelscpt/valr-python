0b34ead582db95da7f89b62d9c528384d8e3c627
jonathanelscpt <jonathanelscpt@gmail.com>
2020-01-12 00:42:20 +0100

    Bump version: 0.1.6 → 0.1.7

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name='valr-python',
-    version='0.1.6',
+    version='0.1.7',
     license='MIT',
     description='Python SDK for the VALR REST API',
     long_description='%s\n%s' % (
